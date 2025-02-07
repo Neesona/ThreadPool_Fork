5a5b3686bd652c9df2e85c8f497110f5efb0dd95
Neeson <Neeson@unknown.com>
2025-02-07 21:57:37 +0800

    增加测试例子

--- example.cpp
+++ example.cpp
@@ -4,6 +4,19 @@
 
 #include "ThreadPool.h"
 
+int GetMaxValue(const std::vector<int>& nums, int start, int end)
+{
+    int ret = INT_MIN;
+    for (int i = start; i <= end; ++i)
+    {
+        if (nums[i] > ret)
+        {
+            ret = nums[i];
+        }
+    }
+    return ret;
+}
+
 int main()
 {
     
@@ -26,6 +39,62 @@ int main()
     for(auto && result: results)
         std::cout << result.get() << ' ';
     std::cout << std::endl;
+
+    // 例子，随机生成n个数，找出最大值
+    int n = 10000000;
+    std::vector<int> nums;
+    std::srand(static_cast<unsigned int>(std::time(0)));
+    for (int i = 0; i < n; ++i)
+    {
+        nums.emplace_back(std::rand() % n + 1);
+    }
+
+    // 单线程 直接比较
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        int ret = INT_MIN;
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] > ret)
+            {
+                ret = nums[i];
+            }
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> elapsed_ms = end - start;
+        std::cout << ret << "  " << elapsed_ms.count() << std::endl;
+    }
+
+
+    // 多线程，先分成m组，分别比较出最大值，然后再比较m个数的最大值
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        int m = 10; // 此处合理分组，过大会导致enqueue构造任务花费大量的时间！
+        int count = n / m;
+        ThreadPool maxPool(5);
+        std::vector<std::future<int>> maxValues;
+        for (int i = 0; i < m; ++i)
+        {
+            maxValues.emplace_back(maxPool.enqueue(GetMaxValue, nums, i * count, (i + 1) * count - 1));
+        }
+
+        auto mid = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> midelapsed = mid - start;
+        std::cout << midelapsed.count() << std::endl;
+
+        int ret = INT_MIN;
+        for (auto&& maxValue : maxValues)
+        {
+            int threadret = maxValue.get();
+            if (threadret > ret)
+            {
+                ret = threadret;
+            }
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> elapsed_ms = end - start;
+        std::cout << ret << "  " << elapsed_ms.count() << std::endl;
+    }
     
     return 0;
 }
